ef0e149ed21e17228c4f9f26e1d09f44cda002f9
Talin <viridia@gmail.com>
2022-06-23 16:29:13 -0700

    Removed eslint/react plugin.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['alloy', 'alloy/react', 'alloy/typescript'],
+  extends: ['alloy', 'alloy/typescript'],
   env: {
     // Your environments (which contains several predefined global variables)
     //
@@ -19,10 +19,6 @@ module.exports = {
     complexity: 'off',
     'no-param-reassign': 'off',
     'max-nested-callbacks': 'off',
-    'react/no-unknown-property': [
-      'error',
-      { ignore: ['class', 'classList', 'stop-color', 'autofocus'] },
-    ],
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/member-ordering': 'off',
     '@typescript-eslint/no-parameter-properties': 'off',
